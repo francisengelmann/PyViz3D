cf643279d63d62a01b8f10e7f328bb2fec29b727
Francis Engelmann <francis.engelmann@gmail.com>
2018-05-19 23:06:07 -0700

    pyviz3d: add initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyviz3d',
+      version='0.1',
+      description='PyViz3D is a Python package to visualize 3D point clouds.',
+      url='https://github.com/francisengelmann/pyviz3d',
+      author='Francis Engelmann',
+      author_email='francis.engelmann@gmail.com',
+      license='MIT',
+      packages=['pyviz3d'],
+      zip_safe=False)
