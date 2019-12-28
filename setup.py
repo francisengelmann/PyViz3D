f0c9916ffea95a8ffe6eb8be29fdafc0be76af3c
Francis Engelmann <francis.engelmann@gmail.com>
2019-12-28 20:04:20 +0100

    Upadate version number.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='pyviz3d',
-      version='0.1.1',
+      version='0.1.2',
       description='PyViz3D is a Python package to visualize 3D point clouds.',
       long_description='PyViz3D is a Python package to visualize 3D point clouds.',
       url='https://github.com/francisengelmann/pyviz3d',
-      download_url='https://github.com/francisengelmann/pyviz3d/archive/0.1.tar.gz',
+      download_url='https://github.com/francisengelmann/pyviz3d/archive/0.1.2.tar.gz',
       author='Francis Engelmann',
       author_email='francis.engelmann@gmail.com',
       license='MIT',
