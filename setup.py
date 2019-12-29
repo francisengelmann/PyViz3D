8c33ca44e22409f9804098ab40ea2f221a81676d
Francis Engelmann <francis.engelmann@gmail.com>
2019-12-29 22:40:53 +0100

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.1.5'
+version = '0.1.6'
 
 setup(name='pyviz3d',
       version=version,
