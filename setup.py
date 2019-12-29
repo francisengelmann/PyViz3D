5b6ee9036504a2cb6533c56e9df2ffd2e6e9dde0
Francis Engelmann <francis.engelmann@gmail.com>
2019-12-29 21:41:28 +0100

    Update setup.py with long description.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup
 
+# Read the contents of your README file.
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '0.1.4'
+
 setup(name='pyviz3d',
-      version='0.1.2',
+      version=version,
       description='PyViz3D is a Python package to visualize 3D point clouds.',
-      long_description='PyViz3D is a Python package to visualize 3D point clouds.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/francisengelmann/pyviz3d',
-      download_url='https://github.com/francisengelmann/pyviz3d/archive/0.1.2.tar.gz',
+      download_url='https://github.com/francisengelmann/pyviz3d/archive/' + version + '.tar.gz',
       author='Francis Engelmann',
       author_email='francis.engelmann@gmail.com',
       license='MIT',
       packages=['pyviz3d'],
-      install_requires=[
-          'numpy',
-          'vtk',
-      ],
+      install_requires=['numpy', 'vtk'],
       zip_safe=False)
