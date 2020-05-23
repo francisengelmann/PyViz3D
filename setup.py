733be5ee02217bbb5787959378e30186bc2f4e28
Francis Engelmann <engelmann@vision.rwth-aachen.de>
2020-05-23 14:18:43 +0200

    New version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.2.17'
+version = '0.2.18'
 
 setup(name='pyviz3d',
       version=version,
