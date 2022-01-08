fc7899f8308da24ef8cee79fb36d06b1480a80d9
Francis Engelmann <francis.engelmann@gmail.com>
2022-01-08 19:02:17 +0100

    Clean up

--- examples/example_meshes.py
+++ examples/example_meshes.py
@@ -1,14 +1,13 @@
 import pyviz3d.visualizer as viz
-import numpy as np
 import math
 
-def main():
 
+def main():
     v = viz.Visualizer()
     v.add_mesh('Plane', path='examples/data/plane.obj',
-               rotation=[math.pi/2, 0, -math.pi/3], scale=[5,5,5], translation=[-3,-1,1], color=[100, 170, 255])
+               rotation=[math.pi/2, 0, -math.pi/3], scale=[5, 5, 5], translation=[-3, -1, 1], color=[100, 170, 255])
     v.add_mesh('Motorbike', path='examples/data/motorbike.obj',
-               rotation=[math.pi/1.5, 0, 0], scale=[2,2,2], translation=[0, 0.5, 0.5], color=[50, 225, 50])
+               rotation=[math.pi/1.5, 0, 0], scale=[2, 2, 2], translation=[0, 0.5, 0.5], color=[50, 225, 50])
     for i in range(10):
         ii = i / 3.0
         pos_x = math.cos(ii) * ii
@@ -17,7 +16,7 @@ def main():
                    color=[255, i*30, 0],
                    translation=[pos_x, pos_y, 0.0],
                    rotation=[3.1415/2, ii, 0],
-                   scale=[ii/2.0,ii/2.0,ii/2.0])
+                   scale=[ii/2.0, ii/2.0, ii/2.0])
     v.save('example_meshes')
 
 
